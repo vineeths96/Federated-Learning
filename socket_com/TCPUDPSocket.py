6c9372a8ab55dfabc7d3896795af8a8c5f0d239b
vineeths96 <vinsree1996@gmail.com>
2021-05-24 20:43:57 +0530

    Update TCPUDP Socket

--- socket_com/TCPUDPSocket.py
+++ socket_com/TCPUDPSocket.py
@@ -283,6 +283,11 @@ class TCPUDPClient:
 
             time.sleep(self.DELAY)
 
+    def send(self, tensor):
+        self.sendTCP_SOT()
+        self.sendUDP(tensor)
+        self.sendTCP_EOT()
+
     def receive(self):
         buffer = []
         readnext = True
