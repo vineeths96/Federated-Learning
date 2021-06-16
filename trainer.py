c03dfed402ab8a2e42808baa5cfd35b13c4f00e9
vineeths96 <vinsree1996@gmail.com>
2021-06-16 18:05:48 +0530

    Local SGD update fix

--- trainer.py
+++ trainer.py
@@ -185,21 +185,22 @@ def train(local_rank, world_size):
                 epoch_metrics.add(metrics)
 
                 if global_iteration_count % config["local_steps"] == 0:
-                    with timer("batch.accumulate", epoch_frac, verbosity=2):
                         if config["algorithm"] == "local_sgd":
                             with timer("batch.step", epoch_frac, verbosity=2):
                                 optimizer.step()
                                 optimizer.zero_grad()
 
-                            for param, send_buffer in zip(params, send_buffers):
-                                send_buffer[:] = param
+                            with timer("batch.accumulate", epoch_frac, verbosity=2):
+                                for param, send_buffer in zip(params, send_buffers):
+                                    send_buffer[:] = param
 
                             with timer("batch.reduce", epoch_frac):
                                 bits_communicated += reducer.reduce(send_buffers, params)
 
                         elif config["algorithm"] == "distributed_learning":
-                            for grad, send_buffer in zip(grads, send_buffers):
-                                send_buffer[:] = grad
+                            with timer("batch.accumulate", epoch_frac, verbosity=2):
+                                for grad, send_buffer in zip(grads, send_buffers):
+                                    send_buffer[:] = grad
 
                             with timer("batch.reduce", epoch_frac):
                                 bits_communicated += reducer.reduce(send_buffers, grads)
